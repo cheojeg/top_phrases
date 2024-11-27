dc5ae40d1e13d18d07506e168118af1ca2b64e94
Jose Garcia <cheojeg@gmail.com>
2024-11-27 10:23:24 -0400

    Remove old login user web

--- api/server.go
+++ api/server.go
@@ -61,7 +61,6 @@ func (server *Server) setupRouter() {
 	router := gin.Default()
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
-	router.POST("/users/login_web", server.loginUserWeb)
 	router.POST("/tokens/renew_access", server.renewAccessToken)
 
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker, server.store))
